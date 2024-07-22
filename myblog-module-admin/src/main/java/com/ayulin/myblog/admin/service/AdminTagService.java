6113e4e61f4e31f08a627807534246331d64c830
lxy <3056053326@qq.com>
2024-07-22 03:20:10 -0400

    分类和标签管理模块

---
+++ myblog-module-admin/src/main/java/com/ayulin/myblog/admin/service/AdminTagService.java
@@ -0,0 +1,39 @@
+package com.ayulin.myblog.admin.service;
+
+import com.ayulin.myblog.admin.model.vo.tag.AddTagReqVO;
+import com.ayulin.myblog.admin.model.vo.tag.DeleteTagReqVO;
+import com.ayulin.myblog.admin.model.vo.tag.FindTagPageListReqVO;
+import com.ayulin.myblog.admin.model.vo.tag.SearchTagsReqVO;
+import com.ayulin.myblog.common.utils.PageResponse;
+import com.ayulin.myblog.common.utils.Response;
+
+public interface AdminTagService {
+    /**
+     * 添加标签集合
+     * @param addTagReqVO
+     * @return
+     */
+    Response addTags(AddTagReqVO addTagReqVO);
+
+    /**
+     * 查询标签分页
+     * @param findTagPageListReqVO
+     * @return
+     */
+    PageResponse findTagPageList(FindTagPageListReqVO findTagPageListReqVO);
+
+    /**
+     * 删除标签
+     * @param deleteTagReqVO
+     * @return
+     */
+    Response deleteTag(DeleteTagReqVO deleteTagReqVO);
+
+    /**
+     * 根据标签关键词模糊查询
+     * @param searchTagsReqVO
+     * @return
+     */
+    Response searchTags(SearchTagsReqVO searchTagsReqVO);
+}
+
---
+++ myblog-module-admin/src/main/java/com/ayulin/myblog/admin/service/impl/AdminCategoryServiceImpl.java
@@ -0,0 +1,124 @@
+package com.ayulin.myblog.admin.service.impl;
+
+import com.ayulin.myblog.admin.model.vo.category.AddCategoryReqVO;
+import com.ayulin.myblog.admin.model.vo.category.DeleteCategoryReqVO;
+import com.ayulin.myblog.admin.model.vo.category.FindCategoryPageListReqVO;
+import com.ayulin.myblog.admin.model.vo.category.FindCategoryPageListRspVO;
+import com.ayulin.myblog.admin.service.AdminCategoryService;
+import com.ayulin.myblog.common.domain.dos.CategoryDO;
+import com.ayulin.myblog.common.domain.mapper.CategoryMapper;
+import com.ayulin.myblog.common.enums.ResponseCodeEnum;
+import com.ayulin.myblog.common.exception.BizException;
+import com.ayulin.myblog.common.model.vo.SelectRspVO;
+import com.ayulin.myblog.common.utils.PageResponse;
+import com.ayulin.myblog.common.utils.Response;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@Service
+@Slf4j
+public class AdminCategoryServiceImpl implements AdminCategoryService {
+
+    @Autowired
+    private CategoryMapper categoryMapper;
+
+    /**
+     * 添加分类
+     *
+     * @param addCategoryReqVO
+     * @return
+     */
+    @Override
+    public Response addCategory(AddCategoryReqVO addCategoryReqVO) {
+        String categoryName = addCategoryReqVO.getName();
+
+        // 先判断该分类是否已经存在
+        CategoryDO categoryDO = categoryMapper.selectByName(categoryName);
+
+        if (Objects.nonNull(categoryDO)) {
+            log.warn("分类名称： {}, 此分类已存在", categoryName);
+            throw new BizException(ResponseCodeEnum.CATEGORY_NAME_IS_EXISTED);
+        }
+
+        // 构建 DO 类
+        CategoryDO insertCategoryDO = CategoryDO.builder()
+                .name(addCategoryReqVO.getName().trim())
+                .build();
+
+        // 执行 insert
+        categoryMapper.insert(insertCategoryDO);
+
+        return Response.success();
+    }
+
+    @Override
+    public PageResponse findCategoryPageList(FindCategoryPageListReqVO findCategoryPageListReqVO) {
+        // 获取当前页、以及每页需要展示的数据数量
+        Long current = findCategoryPageListReqVO.getCurrent();
+        Long size = findCategoryPageListReqVO.getSize();
+        String name = findCategoryPageListReqVO.getName();
+        LocalDate startDate = findCategoryPageListReqVO.getStartDate();
+        LocalDate endDate = findCategoryPageListReqVO.getEndDate();
+
+        // 执行分页查询
+        Page<CategoryDO> categoryDOPage = categoryMapper.selectPageList(current, size, name, startDate, endDate);
+
+        List<CategoryDO> categoryDOS = categoryDOPage.getRecords();
+
+        // DO 转 VO
+        List<FindCategoryPageListRspVO> vos = null;
+        if (!CollectionUtils.isEmpty(categoryDOS)) {
+            vos = categoryDOS.stream()
+                    .map(categoryDO -> FindCategoryPageListRspVO.builder()
+                            .id(categoryDO.getId())
+                            .name(categoryDO.getName())
+                            .createTime(categoryDO.getCreateTime())
+                            .build())
+                    .collect(Collectors.toList());
+        }
+
+        return PageResponse.success(categoryDOPage, vos);
+    }
+
+    @Override
+    public Response deleteCategory(DeleteCategoryReqVO deleteCategoryReqVO) {
+        // 分类 ID
+        Long categoryId = deleteCategoryReqVO.getId();
+
+        // 删除分类
+        categoryMapper.deleteById(categoryId);
+
+        return Response.success();
+    }
+
+    @Override
+    public Response findCategorySelectList() {
+        // 查询所有分类
+        List<CategoryDO> categoryDOS = categoryMapper.selectList(null);
+
+        // DO 转 VO
+        List<SelectRspVO> selectRspVOS = null;
+        // 如果分类数据不为空
+        if (!CollectionUtils.isEmpty(categoryDOS)) {
+            // 将分类 ID 作为 Value 值，将分类名称作为 label 展示
+            selectRspVOS = categoryDOS.stream()
+                    .map(categoryDO -> SelectRspVO.builder()
+                            .label(categoryDO.getName())
+                            .value(categoryDO.getId())
+                            .build())
+                    .collect(Collectors.toList());
+        }
+
+        return Response.success(selectRspVOS);
+    }
+}
