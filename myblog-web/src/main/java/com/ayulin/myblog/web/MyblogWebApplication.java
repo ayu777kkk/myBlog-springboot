51e046f01e9f54a48e339018a91ca0f93e4d3dd9
lxy <3056053326@qq.com>
2024-07-27 03:03:10 -0400

    admin后台仪表盘开发完成，共三个接口

--- myblog-web/src/main/java/com/ayulin/myblog/web/MyblogWebApplication.java
+++ myblog-web/src/main/java/com/ayulin/myblog/web/MyblogWebApplication.java
@@ -3,9 +3,11 @@ package com.ayulin.myblog.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ComponentScan({"com.ayulin.myblog.*"}) // 多模块项目中必须手动指定扫描所有类
+@EnableScheduling // 启用定时任务
 public class MyblogWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(MyblogWebApplication.class, args);
